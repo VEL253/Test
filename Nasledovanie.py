902295cd37f4b449e7d56aaf56a881efd54cbce4
VEL253 <HappyWhelss@yandex.ru>
2024-12-27 09:59:38 +0300

    Create Nasledovanie.py
    Здесь находится пример наследного класса.

---
+++ Nasledovanie.py
@@ -0,0 +1,15 @@
+#создаём класс портфель
+class Bag():
+    #создаём класс портфель
+    def __init__(self, color, len_book):
+        #записываем СВОЙСТВА данных(цвет и вместимость)
+        self.color=color
+        self.len_book=len_book
+    def types(self):
+        print(f"Ваш портфель имеет {self.color} цвет.")
+        print(f"Ваш портфель имеет вместимость в {self.len_book} книг.")
+class School(Bag):
+    def __init__(self, color, len_book, int_book):
+        #создаём наследственный класс
+        super().__init__(color, len_book)
+        self.int_book=int_book

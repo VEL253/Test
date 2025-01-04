5e20f1a1f00971d6ae728167d5fb8970e4bc0535
VEL253 <HappyWhelss@yandex.ru>
2025-01-04 15:35:53 +0300

    Update Наследование.py

--- Наследование.py
+++ Наследование.py
@@ -1,1 +1,21 @@
+class Bags():
+    def __init__(self, color, brand):
+        self.color=color
+        self.brand=brand
+    def info(self):
+        print(f"Это {self.color} портфель от бренда {self.brand}!")
 
+A=Bags('салатовый','puma')#Однажды вы приобрели порфель.
+A.info()#Заполнив на сайте его данные, вам чего-то не хватало.
+#Вы захотели заклеймить портфель, введя свои имя и возраст в программу.
+#Вы подумали и написали для этого следущий код:
+
+class Bags_owner(Bags):
+    def __init__(self, color, brand, name, age):#Добавили имя и возраст
+        super().__init__(color, brand)
+        self.name=name
+        self.age=age
+    def info(self):
+        print(f"Это {self.color} портфель от бренда {self.brand}, принадлежащий {self.name} с возрастом в {self.age} лет.")#Так компания подтверждает данные пользователя.
+B=Bags_owner('салатовый','puma','Василий','29')
+B.info()#Итак, в случае пропажи портфеля вы легко сможете его вернуть, предъявив данные компании, чтобы она сверилась с введённые вами ранее!
